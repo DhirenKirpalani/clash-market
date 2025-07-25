412c8ea330dc47d6b1c17ef38f703ebaeda831ba
Noobmaster169 <mariotaning@gmail.com>
2025-07-25 14:12:52 +0800

    Add .next to gitignore

--- phase1/.next/static/chunks/webpack.js
+++ phase1/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b1f1ea51cdf7925"; }
+/******/ 		__webpack_require__.h = function() { return "36ecf48c769c8f15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
