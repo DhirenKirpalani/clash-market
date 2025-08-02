4c5be361a4d9ab86330fe14e73347e8de6a0ccdf
DhirenKirpalani <93252049+DhirenKirpalani@users.noreply.github.com>
2025-08-02 13:25:17 +0700

    Delete phase1 directory

--- phase1/types/phantom.d.ts
+++
@@ -1,9 +0,0 @@
-interface Window {
-  solana?: {
-    isPhantom?: boolean;
-    connect: () => Promise<{ publicKey: { toString: () => string } }>;
-    disconnect: () => Promise<void>;
-    on: (event: 'connect' | 'disconnect' | 'accountChanged', callback: () => void) => void;
-    signMessage?: (message: Uint8Array) => Promise<{ signature: Uint8Array }>;
-  }
-}
